f5cdd43c3f6a1b65c37266bbc3a0ec076f7309b7
Shubham Rachha (Titan Spy) <53960336+Titan-Spy@users.noreply.github.com>
2024-02-21 20:55:29 +0530

    Add files via upload
    1. s, *d = str(number).partition("."): This line converts the given number to a string and then uses the partition method to split it into three parts: the part before the decimal point (s), the decimal point (if present), and the part after the decimal point (d).
    
    2. [s[x-2:x] for x in range(-3, -len(s), -2)][::-1] + [s[-3:]]: This part of the code uses a list comprehension to iterate over the string s in reverse, taking slices of two characters at a time. It starts from the third character from the end (-3) and goes up to the length of s (excluding the decimal point) in steps of -2 (i.e., in reverse order). These slices represent groups of two digits in the number. The list comprehension then reverses the order of these groups ([::-1]) and adds the last three characters of s (representing the digits before the first comma, if any) as a single group at the end.
    
    3. ",".join(...): This joins all the groups of digits with a comma between them.
    
    4. "join([r] + d)": This joins the formatted integer part (r) with the decimal part (d), if any, and returns the final formatted string representing the number in Indian currency notation.

---
+++ int_to_indian_notation Assignment.py
@@ -0,0 +1,6 @@
+def format_int_to_indian_notation(number):
+    s, *d = str(number).partition(".")
+    r = ",".join([s[x-2:x] for x in range(-3, -len(s), -2)][::-1] + [s[-3:]])
+    return "".join([r] + d)
+    
+print(format_int_to_indian_notation(504678))
